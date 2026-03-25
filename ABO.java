ed528b816116878e6417a939a2e1544ea925dac2
kkkantarutty <arthurcantarutti626@gmail.com>
2026-03-24 21:43:22 -0300

    Add files via upload

---
+++ ABO.java
@@ -0,0 +1,14 @@
+
+package abo;
+import java.util.Scanner;
+public class ABO {
+
+    public static void main(String[] args) {
+        System.out.print(" +\"\"\"\"\"+                                                 \n" +
+"[| o o |]                                                \n" +
+" |  ^  |                                                 \n" +
+" | '-' |                                                 \n" +
+" +-----+");
+    }
+    
+}
